bbf30691d0d5146cda5b915151866a8680984e16
Yuya Unno <unnonouno@gmail.com>
2012-10-06 10:50:39 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='mpidl',
+    version='0.1.0',
+    description='msgpack-idl helper library',
+    author='Yuya Unno',
+    author_email='unnonouno@gmail.com',
+    packages=['mpidl']
+    )
